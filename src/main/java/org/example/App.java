3f947e0e0a7fb89f166d29daf297e50521427fdd
patpatdotmp4 <47404725+patpatdotmp4@users.noreply.github.com>
2021-08-31 17:59:04 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Exercise 1 Solution
+ *  Copyright 2021 Patrick Mac
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner x = new Scanner(System.in);
+        System.out.print( "What is the quote? " );
+        String quote = x.nextLine();
+        System.out.print( "Who said it? " );
+        String author = x.nextLine();
+        System.out.println(author+" says, \"" +quote+ "\"");
+    }
+}
